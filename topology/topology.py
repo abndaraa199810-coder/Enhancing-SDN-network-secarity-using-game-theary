ca2e8f0208838f4ec92a6ff78cfa3fd413092f68
abndaraa199810-coder <abndaraa199810@gmail.com>
2026-08-06 00:18:10 +0300

    Add files via upload

--- topology/topology.py
+++ topology/topology.py
@@ -8,26 +8,26 @@ class TwoSwitchSDNTopo(Topo):
         s2 = self.addSwitch('s2')
 
         # Create hosts under switch s1
-        h1 = self.addHost('h1', ip='10.0.0.1/24')  # attacker
-        h2 = self.addHost('h2', ip='10.0.0.2/24')  # normal user
-        h3 = self.addHost('h3', ip='10.0.0.3/24')  # test host
+        h1 = self.addHost('h1', ip='10.0.0.1/24')  # Primary attacker
+        h2 = self.addHost('h2', ip='10.0.0.2/24')  # Secondary attacker (DDoS)
+        h3 = self.addHost('h3', ip='10.0.0.3/24')  # Secondary attacker (DDoS)
 
         # Create hosts under switch s2
-        h4 = self.addHost('h4', ip='10.0.0.4/24')  # victim
-        h5 = self.addHost('h5', ip='10.0.0.5/24')  # normal user
-        h6 = self.addHost('h6', ip='10.0.0.6/24')  # test host
+        h4 = self.addHost('h4', ip='10.0.0.4/24')  # Victim
+        h5 = self.addHost('h5', ip='10.0.0.5/24')  # Legitimate host
+        h6 = self.addHost('h6', ip='10.0.0.6/24')  # Legitimate host
 
-        # Connect hosts to s1
+        # Connect hosts to switch s1
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s1)
 
-        # Connect hosts to s2
+        # Connect hosts to switch s2
         self.addLink(h4, s2)
         self.addLink(h5, s2)
         self.addLink(h6, s2)
 
-        # Inter-switch link
+        # Connect the two switches
         self.addLink(s1, s2)
 
 
